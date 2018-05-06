1afaf41397d4d81072d98298ecff0774892b9568
novem6 <reriei247@gmail.com>
2018-05-07 07:54:07 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/rei/tanaka/javalog/Dog.java
@@ -0,0 +1,22 @@
+package jp.techacademy.rei.tanaka.javalog;
+import android.util.Log;
+
+class Dog {
+    static String to_jp = "犬";
+
+    String name;
+    int age;
+
+    public Dog(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public static void introduce(){
+        Log.d("javatest", "これは犬クラスです");
+    }
+
+    public void say(){
+        Log.d("javatest", this.name + "(" + this.age + "歳)" + "ワンワン");
+    }
+}
---
+++ app/src/main/java/jp/techacademy/rei/tanaka/javalog/Thinkable.java
@@ -0,0 +1,5 @@
+package jp.techacademy.rei.tanaka.javalog;
+
+public interface Thinkable {
+    public void think();
+}
